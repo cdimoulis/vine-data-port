3f15d1ab7dee7bebc93ef3c5980193678c144eab
Christopher Dimoulis <christopherjamesd@gmail.com>
2017-11-27 14:27:27 -0600

    create location type from f1::address_type

--- app/models/f1/address_type.rb
+++ app/models/f1/address_type.rb
@@ -23,4 +23,41 @@ class F1::AddressType < F1::Base
 
   # Total Records
   # 7/29/2017 9
+
+  ####
+  # CIVICRM model mapping
+  ####
+
+  # The mapping to civicrm
+  def civicrm_models
+    # If the name does not exist
+    if !CIVICRM::LocationType.exists?({name: self.name})
+      location_type = location_type_model()
+      if !location_type.valid? || !location_type.save
+        raise "Invalid Location Type Model\nF1::AddressType: #{self.inspect}\nCIVICRM::LocationType: #{location_type.errors.inspect}"
+      end
+    end
+    # in case more needs to be done here
+  end
+
+  # Create the contact model
+  def location_type_model
+    CIVICRM::LocationType.new(
+      name: self.name,
+      display_name: self.name,
+      vcard_name: self.name.upcase,
+      is_reserved: false,
+      is_active: true,
+      is_default: false
+    )
+  end
+
+  # Create all missing models
+  def self.civicrm_create_all
+    start = CIVICRM::LocationType.count
+    F1::AddressType.all.each do |at|
+      at.civicrm_models()
+    end
+    puts "\nCreated: #{CIVICRM::LocationType.count - start} CIVICRM::LocationType records\n"
+  end
 end
