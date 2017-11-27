875523bcd3027015e4bf1dcd61c6ec0f81ef03fe
Christopher Dimoulis <christopherjamesd@gmail.com>
2017-11-27 12:42:40 -0600

    CIVCRM consistent id type

--- db_civicrm/migrate/20171127170428_create_civicrm_contact_prev_id.rb
+++ db_civicrm/migrate/20171127170428_create_civicrm_contact_prev_id.rb
@@ -1,6 +1,7 @@
 class CreateCivicrmContactPrevId < ActiveRecord::Migration
   def change
-    create_table :civicrm_contact_prev_id do |t|
+    create_table :civicrm_contact_prev_id, id: false do |t|
+      t.column :id, 'integer unsigned auto_increment PRIMARY KEY'
       t.column :contact_id, 'integer unsigned', null: false
       t.column :f1_id, 'integer unsigned'
       t.column :alf_id, 'integer unsigned'
